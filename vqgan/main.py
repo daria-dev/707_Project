dec2880c8f413b6ad61264cfe55554bfa8cc6501
daria-dev <dmashano@andrew.cmu.edu>
2022-04-11 10:35:24 -0400

    data loading

---
+++ vqgan/main.py
@@ -0,0 +1,46 @@
+#from vqgan import VQGAN
+from util import get_data_loader, imshow
+import argparse
+import matplotlib.pyplot as plt
+
+def main(args):
+    # load images
+    dataloader = get_data_loader(args.dataset, args)
+
+    # interative MPL
+    plt.ion()
+
+    for batch in dataloader:
+        real_images, labels = batch
+
+        plt.figure()
+        imshow(real_images[0,:,:,:])
+
+    plt.ioff()
+    plt.show()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="VQGAN")
+    parser.add_argument('--latent-dim', type=int, default=256, help='Latent dimension n_z (default: 256)')
+    parser.add_argument('--image-size', type=int, default=256, help='Image height and width (default: 256)')
+    parser.add_argument('--num-codebook-vectors', type=int, default=1024, help='Number of codebook vectors (default: 256)')
+    parser.add_argument('--beta', type=float, default=0.25, help='Commitment loss scalar (default: 0.25)')
+    parser.add_argument('--image-channels', type=int, default=3, help='Number of channels of images (default: 3)')
+    parser.add_argument('--dataset', type=str, default='', help='Path to data (default: /data)')
+    parser.add_argument('--device', type=str, default="cuda", help='Which device the training is on')
+    parser.add_argument('--batch-size', type=int, default=6, help='Input batch size for training (default: 6)')
+    parser.add_argument('--epochs', type=int, default=100, help='Number of epochs to train (default: 50)')
+    parser.add_argument('--learning-rate', type=float, default=2.25e-05, help='Learning rate (default: 0.0002)')
+    parser.add_argument('--beta1', type=float, default=0.5, help='Adam beta param (default: 0.0)')
+    parser.add_argument('--beta2', type=float, default=0.9, help='Adam beta param (default: 0.999)')
+    parser.add_argument('--disc-start', type=int, default=10000, help='When to start the discriminator (default: 0)')
+    parser.add_argument('--disc-factor', type=float, default=1., help='')
+    parser.add_argument('--rec-loss-factor', type=float, default=1., help='Weighting factor for reconstruction loss.')
+    parser.add_argument('--perceptual-loss-factor', type=float, default=1., help='Weighting factor for perceptual loss.')
+    parser.add_argument('--data_preprocess', type=str, default='basic', help='Method for data pre-processing')
+    parser.add_argument('--ext', type=str, default='*.jpg', help='Extension of data images')
+    parser.add_argument('--num_workers', type=int, default=0, help='The number of threads to use for the DataLoader.')
+
+    args = parser.parse_args()
+    main(args)
--- vqgan/util.py
+++ vqgan/util.py
@@ -1,4 +1,73 @@
 import torch
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+import glob
+import os
+import PIL.Image as Image
+from matplotlib import pyplot as plt
+
+'''
+    Custom Data Loader
+'''
+class CustomDataSet(Dataset):
+    """Load images under folders"""
+    def __init__(self, main_dir, ext='*.jpg', transform=None):
+        self.main_dir = main_dir
+        self.transform = transform
+        all_imgs = glob.glob(os.path.join(main_dir, ext))
+        self.total_imgs = all_imgs
+        print(os.path.join(main_dir, ext))
+        print(len(self))
+
+    def __len__(self):
+        return len(self.total_imgs)
+
+    def __getitem__(self, idx):
+        img_loc = self.total_imgs[idx]
+        image = Image.open(img_loc).convert("RGB")
+        tensor_image = self.transform(image)
+        return tensor_image, 0.
+
+def get_data_loader(data_path, opts):
+    """Creates training and test data loaders.
+    """
+    basic_transform = transforms.Compose([
+        transforms.Resize((268, 182), Image.BICUBIC),
+        transforms.ToTensor()
+    ])
+
+    if opts.data_preprocess == 'basic':
+        train_transform = basic_transform
+    elif opts.data_preprocess == 'deluxe':
+        # todo: add your code here: below are some ideas for your reference
+        load_size = int(1.1 * (182, 268))
+        osize = [load_size, load_size]
+        train_transform = transforms.Compose([
+            transforms.Resize(osize, Image.BICUBIC),
+            transforms.RandomCrop((182, 268)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+    dataset = CustomDataSet(os.path.join('data/', data_path), opts.ext, train_transform)
+    dloader = DataLoader(dataset=dataset, batch_size=opts.batch_size, shuffle=True, num_workers=opts.num_workers)
+
+    return dloader
+
+'''
+    For Displaying Images
+'''
+unloader = transforms.ToPILImage()  # reconvert into PIL image
+
+def imshow(tensor, title=None):
+    image = tensor.cpu().clone()  # we clone the tensor to not do changes on it
+    image = image.squeeze(0)      # remove the fake batch dimension
+    image = unloader(image)
+    plt.imshow(image)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
 
 '''
     Swish layer
